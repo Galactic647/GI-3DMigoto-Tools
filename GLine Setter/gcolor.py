d4be1b3b800d28a6140ff2a36f43112b832c4473
Galactic647 <44773161+Galactic647@users.noreply.github.com>
2022-11-14 23:16:32 +0800

    Merge mods support

--- GLine Setter/gcolor.py
+++ GLine Setter/gcolor.py
@@ -10,6 +10,7 @@ from typing import Union
 import configparser
 import logging
 import glob
+import os
 
 colorama.init()
 
@@ -47,46 +48,81 @@ stream_handler.setFormatter(CustomFormatter())
 logger.addHandler(stream_handler)
 
 
-def get_file(type_: str) -> Union[str, None]:
+def get_file(type_: str) -> Union[str, list, None]:
     files = None
     type_ = type_.lower()
 
+    search = ''
+    if os.path.exists('merged.ini'):
+        search = '**/'
+
     if type_ == 'texcoord':
-        files = glob.glob('*Texcoord.buf', recursive=False)
+        files = glob.glob(f'{search}*Texcoord.buf', recursive=False)
     elif type_ == 'ini':
-        files = glob.glob('*.ini', recursive=False)
+        files = glob.glob(f'{search}*.ini', recursive=False)
 
     if not files:
         logger.error(f'Unable to find {type_} file')
         return
-    elif len(files) > 1:
+    elif len(files) > 1 and not search:
         logger.error(f'More than one {type_} file detected')
         return
+    elif search:
+        seen = set()
+        dupes = list()
 
+        for path, _ in map(os.path.split, files):
+            if path in seen:
+                dupes.append(path)
+            else:
+                seen.add(path)
+        
+        if dupes:
+            for dupe in dupes:
+                logger.error(f'More than one {type_} file detected in {dupe}')
+            return
+
+    if search:
+        for file in files:
+            logger.info(f'{type_} file detected -> {file}')
+        return files
     logger.info(f'{type_} file detected -> {files[0]}')
     return files[0]
 
 
-def main() -> None:
+def start(colors: dict, ini: str, texcoord: str) -> None:
     config = configparser.ConfigParser()
+    try:
+        config.read(ini)
+        stride = int(config.get('Resource{0}'.format(texcoord.split('\\')[-1][:-4]), 'stride'))
+        logger.info(f'Stride: {stride}')
+    except Exception as e:
+        logger.error(e)
+        return
+
+    with open(texcoord, 'rb+') as file:
+        logger.info('Setting Color Values...')
+        data = bytearray(file.read())
+
+        for index in range(0, len(data), stride):
+            for offset, value in enumerate(colors.values()):
+                if value is not None:
+                    data[index + offset] = value
+        
+        logger.info('Saving...')
+        file.seek(0)
+        file.write(data)
+        file.truncate()
+        file.close()
+
+
+def main() -> None:
     colors = {
         'R': None,
         'G': None,
         'B': None,
         'A': None
     }
-    texcoord = get_file('texcoord')
-    ini = get_file('ini')
-
-    if ini is None or texcoord is None:
-        return
-
-    try:
-        config.read(ini)
-        stride = int(config.get(f'Resource{texcoord[:-4]}', 'stride'))
-        logger.info(f'Stride: {stride}')
-    except Exception as e:
-        logger.error(e)
 
     for color in colors:
         value = input(f'{color} color value (0 - 255, Default none): ')
@@ -109,20 +145,22 @@ def main() -> None:
     for color, value in colors.items():
         logger.info(f'Set color {color} value: {value}')
 
-    with open(texcoord, 'rb+') as file:
-        logger.info('Setting Color Values...')
-        data = bytearray(file.read())
-
-        for index in range(0, len(data), stride):
-            for offset, value in enumerate(colors.values()):
-                if value is not None:
-                    data[index + offset] = value
+    # Separated if statement to avoid confusing log output
+    ini = get_file('ini')
+    if ini is None:
+        return
         
-        logger.info('Saving...')
-        file.seek(0)
-        file.write(data)
-        file.truncate()
-        file.close()
+    texcoord = get_file('texcoord')
+    if texcoord is None:
+        return
+
+    if isinstance(ini, str):
+        start(colors, ini, texcoord)
+    elif isinstance(ini, list):
+        for i, t in zip(ini, texcoord):
+            logger.info(f'Processing {t}')
+            start(colors, i, t)
+
     logger.info('Done!')
 
 
--- GLine Setter/gline.py
+++ GLine Setter/gline.py
@@ -9,6 +9,7 @@ from typing import Union
 import configparser
 import logging
 import glob
+import os
 
 colorama.init()
 
@@ -46,65 +47,100 @@ stream_handler.setFormatter(CustomFormatter())
 logger.addHandler(stream_handler)
 
 
-def get_file(type_: str) -> Union[str, None]:
+def get_file(type_: str) -> Union[str, list, None]:
     files = None
     type_ = type_.lower()
 
+    search = ''
+    if os.path.exists('merged.ini'):
+        search = '**/'
+
     if type_ == 'texcoord':
-        files = glob.glob('*Texcoord.buf', recursive=False)
+        files = glob.glob(f'{search}*Texcoord.buf', recursive=False)
     elif type_ == 'ini':
-        files = glob.glob('*.ini', recursive=False)
+        files = glob.glob(f'{search}*.ini', recursive=False)
 
     if not files:
         logger.error(f'Unable to find {type_} file')
         return
-    elif len(files) > 1:
+    elif len(files) > 1 and not search:
         logger.error(f'More than one {type_} file detected')
         return
+    elif search:
+        seen = set()
+        dupes = list()
 
+        for path, _ in map(os.path.split, files):
+            if path in seen:
+                dupes.append(path)
+            else:
+                seen.add(path)
+        
+        if dupes:
+            for dupe in dupes:
+                logger.error(f'More than one {type_} file detected in {dupe}')
+            return
+
+    if search:
+        for file in files:
+            logger.info(f'{type_} file detected -> {file}')
+        return files
     logger.info(f'{type_} file detected -> {files[0]}')
     return files[0]
 
 
-def main() -> None:
+def start(thickness: int, ini: str, texcoord: str) -> None:
     config = configparser.ConfigParser()
+    try:
+        config.read(ini)
+        stride = int(config.get('Resource{0}'.format(texcoord.split('\\')[-1][:-4]), 'stride'))
+        logger.info(f'Stride: {stride}')
+    except Exception as e:
+        logger.error(e)
+        return
+
+    with open(texcoord, 'rb+') as file:
+        logger.info('Starting...')
+        data = bytearray(file.read())
+
+        for index in range(0, len(data), stride):
+            data[index + 3] = thickness
+        
+        logger.info('Saving...')
+        file.seek(0)
+        file.write(data)
+        file.truncate()
+        file.close()
+
+
+def main() -> None:
     try:
         thickness = input('Set outline thickness (0-255): ')
         thickness = int(thickness)
     except ValueError:
         logger.error(f'Invalid thickness value of {thickness}')
         return
-        
-    ini = get_file('ini')
-    texcoord = get_file('texcoord')
-
-    if ini is None or texcoord is None:
-        return
-    elif thickness < 0 or thickness > 255:
+    if thickness < 0 or thickness > 255:
         logger.error(f'Invalid thickness of {thickness}, choose between 0 and 255')
         return
     logger.info(f'Thickness set: {thickness}')
+        
+    # Separated if statement to avoid confusing log output
+    ini = get_file('ini')
+    if ini is None:
+        return
+        
+    texcoord = get_file('texcoord')
+    if texcoord is None:
+        return
     
-    try:
-        config.read(ini)
-        stride = int(config.get(f'Resource{texcoord[:-4]}', 'stride'))
-        logger.info(f'Stride: {stride}')
-    except Exception as e:
-        logger.error(e)
-    else:
-        with open(texcoord, 'rb+') as file:
-            logger.info('Starting...')
-            data = bytearray(file.read())
-
-            for index in range(0, len(data), stride):
-                data[index + 3] = thickness
-            
-            logger.info('Saving...')
-            file.seek(0)
-            file.write(data)
-            file.truncate()
-            file.close()
-        logger.info('Done!')
+    if isinstance(ini, str):
+        start(thickness, ini, texcoord)
+    elif isinstance(ini, list):
+        for i, t in zip(ini, texcoord):
+            logger.info(f'Processing {t}')
+            start(thickness, i, t)
+    logger.info('Done!')
 
 
 if __name__ == '__main__':
