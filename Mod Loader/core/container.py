eece0d2a4c815cd477fdbd1c65a3d85cf694faf8
Galactic647 <44773161+Galactic647@users.noreply.github.com>
2023-01-06 03:38:16 +0800

    Add metaclass for container

--- Mod Loader/core/container.py
+++ Mod Loader/core/container.py
@@ -1,7 +1,17 @@
 from __future__ import annotations
 
 
-class CMDContainer(object):
+class CMDContainerMeta(type):
+    def __new__(cls, *args, **kwargs) -> CMDContainerMeta:
+        self = super().__new__(cls, *args)
+
+        return self
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+class CMDContainer(metaclass=CMDContainerMeta):
     def __new__(cls, *args, **kwargs) -> CMDContainer:
         self = super().__new__(cls)
 
