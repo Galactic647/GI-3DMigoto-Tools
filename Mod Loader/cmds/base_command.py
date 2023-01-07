ae5e35a458f5518c6ccf35f1aa0457ed829b1744
Galactic647 <44773161+Galactic647@users.noreply.github.com>
2023-01-08 00:53:43 +0800

    Change import and add several test command [WIP]

--- Mod Loader/cmds/base_command.py
+++ Mod Loader/cmds/base_command.py
@@ -1,52 +1,64 @@
+import core.container as container
+import core.commands as commands
+
 from typing import Optional
-import importlib
-
-commands = importlib.import_module('core.commands', '..')
-container = importlib.import_module('core.container', '..')
 
 
-class BaseCommand(object):
+class BaseCommand(container.CMDContainer):
     def __init__(self, loader: commands.Loader) -> None:
         self.loader = loader
 
     @commands.command()
-    def create(self, slot: str) -> None:
-        pass
+    def create(self, con, slot: str, hidden: Optional[bool] = False) -> None:
+        if slot in self.loader:
+            con.message(f'Slot {slot} already exists')
+            return
+        self.loader.add_slot(name=slot, mods=dict())
+        con.message(f'Creating slot {slot}')
+        con.enter_slot(slot)
     
     @commands.command()
-    def switch(self, slot: str) -> None:
+    def switch(self, con, slot: str) -> None:
         pass
 
     @commands.command()
-    def edit(self, slot: str) -> None:
+    def edit(self, con, slot: str) -> None:
         pass
 
     @commands.command()
-    def remove(self, slot: str) -> None:
+    def remove(self, con, slot: str) -> None:
         pass
 
     @commands.command()
-    def reload(self, slot: Optional[str] = None) -> None:
+    def reload(self, con, slot: Optional[str] = None) -> None:
         pass
 
     @commands.command()
-    def listslot(self) -> None:
+    def listslot(self, con) -> None:
         pass
 
     @commands.command()
-    def activeslot(self) -> None:
+    def activeslot(self, con) -> None:
         pass
 
-    @commands.command()
-    def reloadcmd(self) -> None:
+    @commands.command(scope=commands.UNIVERSAL)
+    def listcommand(self, con: commands.Console) -> None:
+        con.message('\n'.join(self.loader.commands))
+
+    @commands.command(scope=commands.UNIVERSAL)
+    def help(self, con, cmd: Optional[str] = None) -> None:
         pass
 
-    @commands.command()
-    def help(self, cmd: Optional[str] = None) -> None:
-        pass
+    @commands.command(scope=commands.UNIVERSAL)
+    def usage(self, con, cmd) -> None:
+        command = self.loader[cmd]
+        con.message(command.usage)
 
-    @commands.command()
-    def stop(self) -> None:
+    @commands.command(scope=commands.UNIVERSAL, aliases=('exit', 'terminate', 'close'))
+    def stop(self, con, force: Optional[bool] = False) -> None:
+        if con.scope == commands.SLOT and not force:
+            con.message("Cannot exit while in editing mode, use 'force' to force exit")
+            return
         self.loader.stop()
 
 
