f8dc0461393e6d963970322a4142c49958298c14
Galactic647 <44773161+Galactic647@users.noreply.github.com>
2023-03-28 21:00:36 +0800

    Update to latest parser

--- Mod Loader/core/parser.py
+++ Mod Loader/core/parser.py
@@ -1,18 +1,13 @@
-from typing import Optional, TextIO, Iterator, Union
+from typing import Optional, TextIO, Iterator, Union, Any
 from collections.abc import MutableMapping
-import random
-import string
+import contextlib
 import regex
+import uuid
 
 
-def comment_validation(text: str, type_: Optional[str] = None) -> bool:
-    comment_format = '{type_}comment-[a-fA-F0-9]{{16,}}'
-
-    if type_ is None:
-        type_ = ''
-    elif not type_.endswith('-'):
-        type_ = f'{type_}-'
-    if regex.search(comment_format.format(type_=type_), text):
+def identifier_check(name: str, type_: str) -> bool:
+    pattern = r'{type_}-[\da-f]{{8}}(?:-[\da-f]{{4}}){{3}}-[\da-f]{{12}}'
+    if regex.search(pattern.format(type_=type_.lower()), name):
         return True
     return False
 
@@ -28,6 +23,8 @@ class NoSectionError(Exception):
     def __repr__(self) -> str:
         return self.message
 
+    __str__ = __repr__
+
 
 class NoOptionError(Exception):
     """Raised when trying to get non-existent option"""
@@ -39,6 +36,8 @@ class NoOptionError(Exception):
 
     def __repr__(self) -> str:
         return self.message
+    
+    __str__ = __repr__
 
 
 class NoSectionHeaderError(Exception):
@@ -51,6 +50,8 @@ class NoSectionHeaderError(Exception):
 
     def __repr__(self) -> str:
         return self.message
+
+    __str__ = __repr__
 
 
 class ParseError(Exception):
@@ -67,50 +68,63 @@ class ParseError(Exception):
     def __repr__(self) -> str:
         return self.message
 
+    __str__ = __repr__
 
-def get_random_id(k: Optional[int] = 16) -> str:
-    if k < 16:
-        raise ValueError('ID have to have at least 16 digits in length')
-    return ''.join(random.choices(string.hexdigits, k=k))
+
+class Comment(object):
+    PREFIX = (';', '#')
+
+    def __init__(self, comment: str, name: Optional[str] = None, lead_space: Optional[bool] = False,
+                 end_space: Optional[bool] = False) -> None:
+        if name is None:
+            name = f'comment-{uuid.uuid4()}'
+        self._name = name
+
+        if comment.strip()[0] not in self.PREFIX:
+            comment = f'; {comment.strip()}'
+        self._comment = comment.strip()
+
+        self._lead_space = lead_space
+        self._end_space = end_space
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def comment(self) -> str:
+        return self._comment
+
+    def __repr__(self) -> str:
+        prefix = '\n' if self._lead_space else ''
+        suffix = '\n' if self._end_space else ''
+        return f'{prefix}{self._comment}{suffix}'
+
+    __str__ = __repr__
 
 
 class Option(object):
     """Stores key, value pairs of option, or just value if not parsable"""
 
     def __init__(self, **kwargs) -> None:
-        self._name = None
-        self._value = None
         self._is_commented = False
-
-        self.name = kwargs.get('option')
-        if self.name is None:
-            self.name = kwargs.get('name')
         self.value = kwargs.get('value')
+        self.option = kwargs.get('option')
 
-    @property
-    def name(self) -> Union[None, str]:
-        return self._name
-
-    @name.setter
-    def name(self, value: Optional[str] = None) -> None:
-        if value is None or isinstance(value, str):
-            self._name = value
-            return
-        raise TypeError(f"Expected type {'str'!r} got {type(value).__name__!r} instead")
-
-    @property
-    def value(self) -> Union[None, str]:
-        return self._value
-
-    @value.setter
-    def value(self, value: Optional[str] = None) -> None:
-        if value is None or isinstance(value, str):
-            self._value = value
-            return
-        raise TypeError(f"Expected type {'str'!r} got {type(value).__name__!r} instead")
-
-    def set(self, name: Optional[str] = None, value: Optional[str] = None) -> None:
+        name = kwargs.get('name')
+        if name is None:
+            if self.option is None:
+                name = f'item-{uuid.uuid4()}'
+            else:
+                name = self.option
         self.name = name
+        self.inline_comment = kwargs.get('comment')
+
+    def set(self, name: Optional[str] = None, option: Optional[str] = None, value: Optional[str] = None) -> None:
+        if name is not None:
+            self.name = name
+        if option is not None:
+            self.option = option
         self.value = value
 
     def comment(self) -> None:
@@ -124,9 +138,17 @@ class Option(object):
         if self._is_commented:
             prefix = '; '
 
-        if self.value is not None and self.name is None or self.name.startswith('item-'):
-            return f'{prefix}{self.value}'
-        return f'{prefix}{self.name} = {self.value}'
+        value = ''
+        if self.value is not None:
+            value = self.value
+
+        comment = ''
+        if self.inline_comment is not None:
+            comment = self.inline_comment
+
+        if value is not None and identifier_check(self.name, 'item'):
+            return f'{prefix}{value}{comment}'
+        return f'{prefix}{self.option} = {value}{comment}'
 
     __str__ = __repr__
 
@@ -140,36 +162,32 @@ class Section(MutableMapping):
 
     def __init__(self, name: str) -> None:
         self.name = name
-        self.skip_parse = False
+        if 'command' in self.name.lower():
+            self.skip_parse = True
+        else:
+            self.skip_parse = False
         self._options = dict()
 
     @property
     def options(self) -> list:
         return list(self._options)
 
-    def get(self, option: str, *, only_value: Optional[bool] = True):
+    def get(self, option: str, *, only_value: Optional[bool] = True) -> Union[Option, Any]:
         if option not in self:
             raise NoOptionError(option)
         if only_value:
             return self[option].value
         return self[option]
 
-    def add_option(self, option: Union[Option, str] = None, value: Optional[str] = '') -> None:
-        if option is None and not value:
+    def add_option(self, name: Optional[str] = None, option: Optional[str] = None, value: Optional[str] = None, comment: Optional[str] = None) -> None:
+        if option is None is value:
             return
-
-        if isinstance(option, Option):
-            self.update({option.name: option})
-            return
-        if option is None:
-            option = f'item-{get_random_id()}'
-        option = Option(name=option, value=value)
+        option = Option(name=name, option=option, value=value, comment=comment)
         self.update({option.name: option})
 
-    def add_comment(self, comment: str) -> None:
-        if comment.strip()[0] not in ModConfigParser.COMMENT_PREFIX:
-            comment = f'; {comment}'
-        self.update({f'section-comment-{get_random_id()}': comment})
+    def add_comment(self, comment: str, lead_space: Optional[bool] = False, end_space: Optional[bool] = False) -> None:
+        comment = Comment(comment, lead_space=lead_space, end_space=end_space)
+        self.update({comment.name: comment})
 
     def has_option(self, option: str) -> bool:
         return option in self
@@ -184,15 +202,13 @@ class Section(MutableMapping):
             raise KeyError(key)
         return self._options[key]
 
-    def __setitem__(self, key: str, value: Union[Option, str]) -> None:
+    def __setitem__(self, key: str, value: Union[Option, Comment, str]) -> None:
         if key in self._options and self._options[key] == value:
             return
-        elif not isinstance(value, (Option, str)):
+        elif not isinstance(value, (Option, Comment, str)):
             return NotImplemented
 
-        if comment_validation(key, 'section'):
-            self._options[key] = value
-        elif isinstance(value, str):
+        if isinstance(value, str):
             self._options[key] = Option(name=key, value=value)
             return
         self._options[key] = value
@@ -211,13 +227,7 @@ class Section(MutableMapping):
     def __repr__(self) -> str:
         if self.skip_parse or not self._options:
             return f'[{self.name}]'
-        options = list()
-        for key, value in self._options.items():
-            if comment_validation(key, 'section'):
-                options.append(value)
-            else:
-                options.append(str(value))
-        options = '\n'.join(options)
+        options = '\n'.join(map(str, self._options.values()))
         return f'[{self.name}]\n{options}'
 
     __str__ = __repr__
@@ -229,6 +239,14 @@ class ModConfigParser(MutableMapping):
     This parser also works for normal parser but not as rigid as configparser module.
     The parser is able to parse and perserve comments and commands, commands is saved in raw form
     since it was unparseable (intended).
+    
+    Normal usage:
+    >>> config = ModConfigParser()
+    >>> config.read('config.ini')
+
+    To allow multiple option with the same name in a section
+    >>> config.ModConfigParser(restrict=False)
+    >>> config.read('config.ini')
     """
 
     RESECTION = regex.compile(r'^\[(?P<section>.+?)\]$')
@@ -243,27 +261,29 @@ class ModConfigParser(MutableMapping):
         |
             endif               # - Start with endif
         )                       # Match line that
-        (?P<option>[^=]+?)      # - Start with anything that isn't "="
-        \s*=\s*                 # - Have a delimiter of "=" with any amount of whitespaces before and after
-        (?P<value>[^=]+?)       # - End with anything that isn't "="
+        (?P<option>[^=]+?)    # - Anything that isn't =
+        \s*=\s*                 # - = with any number of whitespaces before and after
+        (?P<value>[^;#=]+?)     # - Anything that isn't ;#=
+        \s*                     # - Any number of whitespaces
+        (?P<comment>[;#].*)?    # - Inline comment if any
         $
     """, regex.VERBOSE | regex.IGNORECASE)
-    COMMENT_PREFIX = (';', '#')
 
-    def __init__(self) -> None:
+    def __init__(self, restrict: Optional[bool] = True) -> None:
         self._sections = dict()
+        self.restrict = restrict
 
     @property
     def sections(self) -> list:
         return list(self._sections)
 
-    def set(self, section: str, option: str, value: Optional[str] = '') -> None:
+    def set(self, section: str, option: str, value: Optional[str] = None) -> None:
         if section not in self:
             raise NoSectionError(section)
         self[section].add_option(option, value)
 
     def get(self, section: str, option: Optional[str] = None, *,
-            only_value: Optional[bool] = True) -> Union[Section, None, str]:
+            only_value: Optional[bool] = True) -> Union[Section, Option, None, str]:
         if section not in self:
             raise NoSectionError(section)
         elif option is not None and option not in self[section]:
@@ -275,16 +295,15 @@ class ModConfigParser(MutableMapping):
             return self[section]
         return self[section].get(option, only_value=only_value)
 
-    def add_comment(self, comment: str, section: Optional[str] = None) -> None:
-        if comment.strip()[0] not in self.COMMENT_PREFIX:
-            comment = f'; {comment}'
-
+    def add_comment(self, comment: str, section: Optional[str] = None, lead_space: Optional[bool] = False,
+                    end_space: Optional[bool] = False) -> None:
         if section is None:
-            self.update({f'comment-{get_random_id()}': comment})
+            comment = Comment(comment, lead_space=lead_space, end_space=end_space)
+            self.update({comment.name: comment})
         elif section not in self:
             raise NoSectionError(section)
         else:
-            self[section].add_comment(comment)
+            self[section].add_comment(comment, lead_space=lead_space, end_space=end_space)
 
     def add_section(self, section: Union[Section, str]) -> None:
         if isinstance(section, Section):
@@ -298,7 +317,11 @@ class ModConfigParser(MutableMapping):
     def has_option(self, section: str, option: str) -> bool:
         if section not in self:
             raise NoSectionError(section)
-        return option in self[section]
+        
+        check_section = self[section]
+        if isinstance(check_section, Comment):
+            return option == check_section.name
+        return option in check_section
 
     def remove_section(self, section: str) -> None:
         if section not in self:
@@ -321,17 +344,21 @@ class ModConfigParser(MutableMapping):
         config_data = [line.replace('\n', '') for line in fp.readlines()]
         cursect = None  # None or Section
         lastsect = None  # None or Section
+        lastempty = False  # True if the previous item is space
 
         for line in config_data:
             if not line:
+                lastempty = True
                 continue
-            if line.strip()[0] in self.COMMENT_PREFIX:
+            if line.strip()[0] in Comment.PREFIX:
                 if cursect is None:
-                    self.add_comment(line.strip())
+                    self.add_comment(line.strip(), lead_space=lastempty)
                 else:
-                    self.add_comment(line.strip(), cursect.name)
+                    self.add_comment(line.strip(), cursect.name, lead_space=lastempty)
+                lastempty = False
                 continue
 
+            lastempty = False
             section_match = self.RESECTION.match(line.strip())
             option_match = self.REOPTION.match(line)
 
@@ -340,9 +367,6 @@ class ModConfigParser(MutableMapping):
             elif section_match and option_match is None:
                 section = Section(section_match.group('section'))
 
-                if 'command' in section.name.lower():
-                    section.skip_parse = True
-                
                 if not self.has_section(section.name):
                     lastsect = cursect
                     self.add_section(section)
@@ -352,16 +376,18 @@ class ModConfigParser(MutableMapping):
             elif option_match and section_match is None:
                 if cursect is None is lastsect:
                     raise NoSectionHeaderError(option_match.group('option'))
-                elif cursect is None is not lastsect:
+                elif cursect is None is not lastsect:  # Option of duplicated sections
                     continue
                 elif cursect.skip_parse:
                     cursect.add_option(value=line)
                     continue
 
-                option = Option(**option_match.groupdict())
-                if not cursect.has_option(option.name):
-                    cursect.add_option(option)
-            elif section_match is None is option_match:
+                if not cursect.has_option(option_match.group('option')):
+                    cursect.add_option(**option_match.groupdict())
+                elif not self.restrict and cursect.get(option_match.group('option')) != option_match.group('value'):
+                    name = f"{option_match.group('option')}-{uuid.uuid4()}"
+                    cursect.add_option(name=name, **option_match.groupdict())
+            elif section_match is None is option_match:  # Unparsable line
                 if cursect is None is lastsect:
                     raise NoSectionHeaderError(option_match.group('option'))
                 elif cursect is None is not lastsect:
@@ -370,24 +396,41 @@ class ModConfigParser(MutableMapping):
                 cursect.skip_parse = True
 
     def write(self, fp: TextIO) -> None:
-        for section in self:
-            if comment_validation(section, None):
-                continue
-            self[section].skip_parse = False
-        fp.write('\n\n'.join(map(str, self.values())))
+        fp.write(self.dumps(include_skip=True))
 
-    def dumps(self) -> str:
-        return '\n\n'.join(map(str, self.values()))
+    @contextlib.contextmanager
+    def _parse_check(self, section: Section, include_skip: Optional[bool] = False) -> None:
+        skip_parse = section.skip_parse
+        if include_skip:
+            section.skip_parse = False
+        yield
+        section.skip_parse = skip_parse
+
+    def dumps(self, include_skip: Optional[bool] = False) -> str:
+        items = []
+        comments  = []
+
+        for name, section in self.items():
+            if identifier_check(name, 'comment'):
+                comments.append(section)
+                continue
+            if comments:
+                items.append('\n'.join(map(str, comments)))
+                comments.clear()
+
+            with self._parse_check(section, include_skip):
+                items.append(str(section))
+        return '\n\n'.join(items)
 
     def __getitem__(self, key: str) -> Section:
         if key not in self._sections:
             raise KeyError(key)
         return self._sections[key]
 
-    def __setitem__(self, key: str, value: Union[Section, str]) -> None:
-        if not isinstance(value, (Section, str)):
+    def __setitem__(self, key: str, value: Union[Section, Comment, str]) -> None:
+        if not isinstance(value, (Section, Comment, str)):
             return NotImplemented
-        elif isinstance(value, str) and not comment_validation(key, None):
+        elif isinstance(value, (str, Comment)) and not identifier_check(key, 'comment'):
             return
         self._sections[key] = value
 
