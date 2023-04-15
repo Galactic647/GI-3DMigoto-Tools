88c504db7e578381b0dddcf4b8368ce4c828817e
Galactic647 <44773161+Galactic647@users.noreply.github.com>
2023-04-15 22:56:24 +0800

    Fix some bug with adding option to section

--- Hash Fixer/core/parser.py
+++ Hash Fixer/core/parser.py
@@ -280,7 +280,7 @@ class ModConfigParser(MutableMapping):
     def set(self, section: str, option: str, value: Optional[str] = None) -> None:
         if section not in self:
             raise NoSectionError(section)
-        self[section].add_option(option, value)
+        self[section].add_option(option=option, value=value)
 
     def get(self, section: str, option: Optional[str] = None, *,
             only_value: Optional[bool] = True) -> Union[Section, Option, None, str]:
